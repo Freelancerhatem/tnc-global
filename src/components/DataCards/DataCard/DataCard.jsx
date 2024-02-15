f5265eeb29e4fd48a39530911b9c47c9f5be484d
Freelancerhatem <108505344+Freelancerhatem@users.noreply.github.com>
2024-02-15 08:39:16 +0600

    card designed

--- src/components/DataCards/DataCard/DataCard.jsx
+++ src/components/DataCards/DataCard/DataCard.jsx
@@ -1,19 +1,20 @@
 
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { FaArrowAltCircleRight } from "react-icons/fa";
 const DataCard = ({ userData }) => {
     const { id, image, firstName, lastName, email, phone } = userData;
     return (
         <div>
-            {id}
-            <div className='  bg-white rounded-md shadow-2xl'>
-                <div className='w-14 h-14 rounded-full mx-auto py-2 my-2  bg-white'><img className='w-full  h-full' src={image} alt="" /></div>
+            
+            <div className='  bg-white py-5 rounded-md shadow-2xl'>
+                <div className='w-20 h-20 rounded-full mx-auto p-4 my-2  bg-fuchsia-500'><img className='w-full  h-full' src={image} alt="" /></div>
                 <div className='text-center text-lg font-semibold'><h2>{firstName} {lastName}</h2></div>
-                <div className='flex gap-2 justify-center'>
+                <div className='flex flex-col gap-2 items-center'>
                     <h2>{email} </h2>
                     <h2>{phone}</h2>
                 </div>
-                <Link to={`/user/${id}`}><div className='flex justify-center'><button className="btn btn-xs font-semibold text-center">See More</button></div></Link>
+                <Link to={`/user/${id}`} className='flex justify-center'><button className="flex  justify-center items-center gap-2 text-base px-4 text-white hover:bg-fuchsia-400 rounded-sm py-2 bg-fuchsia-500 font-semibold text-center">See More<FaArrowAltCircleRight/></button></Link>
             </div>
         </div>
     );
--- src/components/DataCards/DataCards.jsx
+++ src/components/DataCards/DataCards.jsx
@@ -10,8 +10,12 @@ const DataCards = () => {
     else {
         const user = data.users;
         return (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-5">
-                {user.map((userData,index)=><DataCard key={index} userData={userData}></DataCard>)}
+            <div className="mt-5">
+                <h1 className="text-center font-bold text-2xl">All User</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-5 py-2">
+
+                    {user.map((userData, index) => <DataCard key={index} userData={userData}></DataCard>)}
+                </div>
             </div>
         );
     }
